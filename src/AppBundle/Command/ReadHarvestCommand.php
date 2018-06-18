385005e7863349fd3a43e7e6e47f46f76b07e0a8
Ture Gjørup <ture@ignatz.dk>
2018-06-18 12:56:53 +0200

    SALG-404: Update console command to use locks to prevent running in parallel.

--- src/AppBundle/Command/ReadHarvestCommand.php
+++ src/AppBundle/Command/ReadHarvestCommand.php
@@ -2,23 +2,11 @@
 
 namespace AppBundle\Command;
 
-use AppBundle\Entity\Harvest\User;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\PropertyAccess\PropertyAccess;
-
-use GuzzleHttp\Client;
-use GuzzleHttp\Promise;
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Exception\RequestException;
-
-use Exception;
-use ReflectionClass;
-use Doctrine\Common\Annotations\AnnotationReader;
-
+use Symfony\Component\Lock\Factory;
+use Symfony\Component\Lock\Store\SemaphoreStore;
 use AppBundle\Service\HarvestApiService;
 
 class ReadHarvestCommand extends ContainerAwareCommand {
@@ -38,13 +26,24 @@ class ReadHarvestCommand extends ContainerAwareCommand {
 			->setDescription( 'Update All Harvest Data' )
 			// the full command description shown when running the command with
 			// the "--help" option
-			->setHelp( "This command updates all data from Harvest via the Harvest v2 API" );
+			->setHelp( 'This command updates all data from Harvest via the Harvest v2 API' );
 	}
 
 	protected function execute( InputInterface $input, OutputInterface $output ) {
+        $store = new SemaphoreStore();
+        $factory = new Factory($store);
 
-		$this->harvestApiService->update( $output );
+        $lock = $factory->createLock('app-harvest-update');
 
+        if ($lock->acquire()) {
+            $this->harvestApiService->update( $output );
+
+            $lock->release();
+        } else {
+            $output->writeln('This command is already running in another process.');
+
+            return 0;
+        }
 	}
 
 
